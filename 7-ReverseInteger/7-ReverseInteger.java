1cccc383d1eeacb17db0bdde56b967776d549cf0
MYDHILI S <mydhili772007@gmail.com>
2026-02-03 16:09:29 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -0,0 +1,14 @@
+// Last updated: 03/02/2026, 16:09:40
+class Solution {
+    public int reverse(int x) {
+        long  rev=0;
+        while(x!=0){
+            rev=rev*10 + x%10;
+            x/=10;
+        }
+        if(rev>Integer.MAX_VALUE || rev < Integer.MIN_VALUE){
+            return 0;
+        } 
+        return (int)rev;
+    }
+}
