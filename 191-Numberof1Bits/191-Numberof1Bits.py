cf3f768950b80ea212b20ece3d12c1f25950856c
MYDHILI S <mydhili772007@gmail.com>
2026-06-30 09:55:03 +0530

    File updated at 30/06/2026, 09:55:03

--- 191-Numberof1Bits/191-Numberof1Bits.py
+++ 191-Numberof1Bits/191-Numberof1Bits.py
@@ -1,5 +1,17 @@
-# Last updated: 30/06/2026, 09:20:16
-1class Solution(object):
-2    def hammingWeight(self, n):
-3        return bin(n).count("1")
-4        
+# Last updated: 30/06/2026, 09:55:03
+1class Solution:
+2    def totalHammingDistance(self, nums):
+3        total = 0
+4        n = len(nums)
+5
+6        for i in range(32):
+7            ones = 0
+8
+9            for num in nums:
+10                if (num >> i) & 1:
+11                    ones += 1
+12
+13            zeros = n - ones
+14            total += ones * zeros
+15
+16        return total
